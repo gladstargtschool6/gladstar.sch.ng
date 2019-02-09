102b2a775d2ee0e4180b7fa9567732b4e767e1c8
Joseph Moshood <37759776+gladstargtschool6@users.noreply.github.com>
2019-02-09 20:00:32 +0100

    Update image-list.js

--- image-list.js
+++ image-list.js
@@ -3,7 +3,7 @@ var Path = 'gallery/';
 var Gallery = { 'images' : [
       
   {
-    'name'  : 'gladstargtschool-logo',
+    'name'  : '10609574_518313521646130_877050346121553165_n.jpg',
     'alt' : 'gladstargtschool-logo',
     'url': 'https://drive.google.com/open?id=0B9HD_WWLE7riWnd4MFBIMVhZOVU',
     'credit': '<a href="https://drive.google.com/open?id=0B9HD_WWLE7riWnd4MFBIMVhZOVU/">gladstar</a>, published under a <a href="https://creativecommons.org/licenses/by-nc-nd/2.0/">Attribution-NonCommercial-NoDerivs 2.0 Generic</a> license.'

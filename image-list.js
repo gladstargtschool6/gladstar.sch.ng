cea5dcd48f9946a8dacab9b8983fef967dc2f2e5
Joseph Moshood <37759776+gladstargtschool6@users.noreply.github.com>
2019-02-09 19:53:16 +0100

    Create image-list.js

---
+++ image-list.js
@@ -0,0 +1,26 @@
+var Path = 'gallery/';
+
+var Gallery = { 'images' : [
+      
+  {
+    'name'  : 'gladstargtschool logo',
+    'alt' : 'gladstargtschool logo',
+    'url': 'https://drive.google.com/open?id=0B9HD_WWLE7riWnd4MFBIMVhZOVU',
+    'credit': '<a href="https://drive.google.com/open?id=0B9HD_WWLE7riWnd4MFBIMVhZOVU/">gladstar</a>, published under a <a href="https://creativecommons.org/licenses/by-nc-nd/2.0/">Attribution-NonCommercial-NoDerivs 2.0 Generic</a> license.'
+  },
+
+  {
+    'name'  : 'gladstargtschool snapshot',
+    'alt' : 'gladstargtschool snapshot',
+    'url': 'https://drive.google.com/open?id=1lNP5CvVjbGA-Do_RY1ct4tZaKbgyfrjO',
+    'credit': '<a href="https://drive.google.com/open?id=1lNP5CvVjbGA-Do_RY1ct4tZaKbgyfrjO"</a>, published under a <a href="https://creativecommons.org/licenses/by-nc-nd/2.0/">Attribution-NonCommercial-NoDerivs 2.0 Generic</a> license.'
+  },
+
+  {
+    'name'  : 'GladstarLeadership app',
+    'alt' : 'GladstarLeadership app',
+    'url': https://drive.google.com/open?id=0B9HD_WWLE7riRnlUSUU2ekJ4SU0'',
+    'credit': '<a href="https://drive.google.com/open?id=0B9HD_WWLE7riRnlUSUU2ekJ4SU0"</a>, published under a <a href="https://creativecommons.org/licenses/by-nc-nd/2.0/">Attribution-NonCommercial-NoDerivs 2.0 Generic</a> license.'
+  },
+  
+]};
